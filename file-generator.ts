70fc81b9ba4fa13cc881772e7ed2c768a66d1d04
Felipe <pipecalgaro@gmail.com>
2025-01-10 22:33:03 -0300

    feat: implement file generator class

---
+++ file-generator.ts
@@ -0,0 +1,68 @@
+import fs from "node:fs";
+import { RawSchema } from "./raw";
+import { TYPES_MAPPER } from "./mapper";
+import { SQLMigrationWriter } from "./sql-migration-writer";
+
+export class FileGenerator {
+  private _schema: RawSchema;
+
+  private _generateDirectories() {
+    if (!fs.existsSync("generated")) {
+      fs.mkdirSync("generated");
+    }
+
+    if (!fs.existsSync("generated/types")) {
+      fs.mkdirSync("generated/types");
+    }
+
+    if (!fs.existsSync("generated/migrations")) {
+      fs.mkdirSync("generated/migrations");
+    }
+  }
+
+  constructor(schema: RawSchema) {
+    this._schema = schema;
+
+    this._generateDirectories();
+  }
+
+  public generateTypesFile() {
+    let fileContent = "";
+
+    Object.entries(this._schema).forEach(
+      ([tableName, columns], index, array) => {
+        fileContent += `export type ${`${tableName
+          .charAt(0)
+          .toUpperCase()}${tableName.slice(1)}Data`} = {\n`;
+
+        Object.entries(columns).forEach(([field, value]) => {
+          if (typeof value == "string") {
+            fileContent += `  ${field}: ${TYPES_MAPPER[value]}\n`;
+          } else {
+            fileContent += `  ${field}: ${TYPES_MAPPER[value.type]}\n`;
+          }
+        });
+
+        fileContent += "}\n";
+        if (index < array.length - 1) fileContent += "\n";
+      }
+    );
+
+    fs.writeFile("generated/types/schema.ts", fileContent, (err) => {
+      if (err) throw err;
+    });
+  }
+
+  public generateSQLFile() {
+    const fileContent = SQLMigrationWriter.generateCreateTableClause(
+      this._schema
+    )
+      .trim()
+      .split(";")
+      .join(";\n\n");
+
+    fs.writeFile("generated/migrations/schema.sql", fileContent, (err) => {
+      if (err) throw err;
+    });
+  }
+}
--- schema.ts
+++ schema.ts
@@ -1,26 +1,10 @@
-import fs from "node:fs";
 import { RawSchema } from "./raw";
-import { TYPES_MAPPER } from "./mapper";
-import { SQLMigrationWriter } from "./sql-migration-writer";
+import { FileGenerator } from "./file-generator";
 
 export class Schema {
   constructor(private _schema: RawSchema = {}) {}
 
-  private _generateDirectories() {
-    if (!fs.existsSync("generated")) {
-      fs.mkdirSync("generated");
-    }
-
-    if (!fs.existsSync("generated/types")) {
-      fs.mkdirSync("generated/types");
-    }
-
-    if (!fs.existsSync("generated/migrations")) {
-      fs.mkdirSync("generated/migrations");
-    }
-  }
-
-  public add(schema: RawSchema) {
+  public define(schema: RawSchema) {
     this._schema = Object.assign(this._schema, schema);
   }
 
@@ -28,48 +12,10 @@ export class Schema {
     return this._schema;
   }
 
-  public generateTypesFile() {
-    let fileContent = "";
+  public migrate() {
+    const fileGenerator = new FileGenerator(this._schema);
 
-    Object.entries(this._schema).forEach(
-      ([tableName, columns], index, array) => {
-        fileContent += `export type ${`${tableName
-          .charAt(0)
-          .toUpperCase()}${tableName.slice(1)}Schema`} = {\n`;
-
-        Object.entries(columns).forEach(([field, value]) => {
-          if (typeof value == "string") {
-            fileContent += `  ${field}: ${TYPES_MAPPER[value]}\n`;
-          } else {
-            fileContent += `  ${field}: ${TYPES_MAPPER[value.type]}\n`;
-          }
-        });
-
-        fileContent += "}\n";
-        if (index < array.length - 1) fileContent += "\n";
-      }
-    );
-
-    fileContent.replace(";", ";\n\n");
-
-    this._generateDirectories();
-
-    fs.writeFile("generated/types/schema.ts", fileContent, (err) => {
-      if (err) throw err;
-    });
-  }
-
-  public generateSQLFile() {
-    const fileContent = SQLMigrationWriter.generateCreateTableClause(
-      this._schema
-    )
-      .trim()
-      .replace(";", ";\n\n");
-
-    this._generateDirectories();
-
-    fs.writeFile("generated/migrations/schema.sql", fileContent, (err) => {
-      if (err) throw err;
-    });
+    fileGenerator.generateSQLFile();
+    fileGenerator.generateTypesFile();
   }
 }
